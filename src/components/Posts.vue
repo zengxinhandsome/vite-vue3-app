d2c1b44e4f034525a86d58c6334bdf2b2b63ce9e
DaWenXi <zengxin@bianfeng.com>
2022-01-19 20:47:16 +0800

    first commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+let posts = ref([]);
+
+const baseUrl = 'https://express-typeorm-demo.herokuapp.com'
+
+axios({
+  url: `${baseUrl}/posts`,
+  method: 'GET'
+}).then(({ data }) => {
+  posts.value = data;
+});
+
+// axios({
+//   url: 'http://localhost:3001/posts/update/2',
+//   method: 'PUT',
+//   data: {
+//     title: '修改 title',
+//     content: '修改 content'
+//   }
+// }).then(({ data }) => {
+//   console.log('data: ', data);
+// });
+
+// axios({
+//   url: 'http://localhost:3001/post/create',
+//   method: 'POST',
+//   data: {
+//     title: 'My first title',
+//     content: 'post content'
+//   }
+// }).then(({ data }) => {
+//   console.log('data: ', data);
+// });
+</script>
+
+<template>
+  <div>{{ posts }}</div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+require('dotenv').config();
+
+createApp(App).mount('#app');
